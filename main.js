77916445cce619a215bebb306bdb5eb48aa5853a
Mihir <mihir7293@gmail.com>
2016-06-24 18:36:21 +0530

    Reverting code

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 'use strict';
-var config = require(__dirname + '/config/main.json')
+var config = require(__dirname + '/config/config.js');
 var db = require(__dirname + '/helpers/db.js');
 var crypt = require(__dirname+'/helpers/crypt.js');
 var loginServer = require(__dirname + '/server/LoginServer.js');
